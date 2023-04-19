240b6fef804f89218c9cce85ec81c735d894f1e0
Miguel Villanueva <emvil542@gmail.com>
2023-04-19 14:18:48 +0800

    build: install cors and connect-mongo deps
    feat(BE): add index.js

---
+++ chat-be/index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+const session = require("express-session");
+const socket = require("socket.io");
+const mongoose = require("mongoose");
+
+const MongoStore = require("connect-mongo")(session);
+
+const app = express();
+
+app.use(cors());
+
+mongoose.connect("mongodb+srv://admin:admin@batch230.cqzdm3c.mongodb.net/port-chat?retryWrites=true&w=majority", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+mongoose.connection.once("open", () => console.log("Now connected to Villanueva-Mongo DB Atlas"));
+
+app.listen(process.env.PORT || 4000, () => 
+	{ console.log(`API is now online on port ${process.env.PORT || 4000} `)
+});
+
