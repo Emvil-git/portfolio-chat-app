db7529c155b82a29c973f4018fe4ce8b22d61793
Miguel Villanueva <emvil542@gmail.com>
2023-04-19 15:02:07 +0800

    feat(BE): create auth.js

---
+++ chat-be/auth.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+const secret = "ChatAppAPI";
+
+module.exports.createAccessToken = (user) => {
+    const data = {
+        id: user._id,
+        email: user.email,
+        mobile: user.mobile
+    }
+
+    return jwt.sign(data, secret, {});
+}
+
+module.exports.verify = (request, response, next) => {
+    let token = request.headers.authorization;
+
+    if(token){
+        console.log(token);
+        token = token.slice(7, token.length);
+
+        return jwt.verify(token, secret, (error, data) =>{
+			if(error){
+				return response.send({
+					auth: "Failed. "
+				});
+			}
+			else{
+				next();
+			}
+		})
+    }
+}
+
+module.exports.decode = (token) => {
+    if(token){
+        token = token.slice(7, token.length);
+    }
+
+    return jwt.verify(token, secret, (error, data) => {
+		if(error){
+			return null
+		}
+		else{
+			return jwt.decode(token, {complete:true}).payload
+		}
+	})
+}
